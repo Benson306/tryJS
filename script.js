72b87851aae9902a54e024160e2f23ba4f759846
Benson Ndiwa <bnkimtai@gmail.com>
2022-02-14 17:34:06 +0300

    Toggle in side bar - incomplete. Pending arangement of divs

--- script.js
+++ script.js
@@ -40,9 +40,9 @@ let togglePassword = function(){
 
 }
 
-$toggleCard = false;
+let $toggleCard = false;
 let toggleCard = function(){
-    $card = document.querySelector('.card');
+    let $card = document.querySelector('.card');
     if($toggleCard === false){
         $card.style.width='120%';
         $card.style.margin='40%';
@@ -57,4 +57,125 @@ let toggleCard = function(){
         $toggleCard =false;
     }
     
+}
+let $toggleTab1 = false;
+let tab1 = function(){
+    
+    let $tab1 = document.querySelector('.tab1');
+    let $tab2 = document.querySelector('.tab2');
+    let $tab3 = document.querySelector('.tab3');
+    let $tab4 = document.querySelector('.tab4');
+
+    if($toggleTab1 ===false){
+        $tab1.style.visibility='visible';
+        $tab2.style.visibility='hidden';
+        $tab3.style.visibility='hidden';
+        $tab4.style.visibility='hidden';
+
+        document.querySelector('.tab').value = $tab1;
+        $toggleTab1 = true;
+        $toggleTab2 = false;
+        $toggleTab3 = false;
+        $toggleTab4 = false;
+    }else if($toggleTab1 ===true){
+        $tab1.style.visibility='hidden';
+        $tab2.style.visibility='hidden';
+        $tab3.style.visibility='hidden';
+        $tab4.style.visibility='hidden';
+        $toggleTab1 = false;
+        $toggleTab2 = true;
+        $toggleTab3 = true;
+        $toggleTab4 = true;
+    }
+
+}
+
+let $toggleTab2 = false;
+let tab2 = function(){
+    
+    let $tab1 = document.querySelector('.tab1');
+    let $tab2 = document.querySelector('.tab2');
+    let $tab3 = document.querySelector('.tab3');
+    let $tab4 = document.querySelector('.tab4');
+
+
+    if($toggleTab2 ===false){
+        $tab1.style.visibility='hidden';
+        $tab2.style.visibility='visible';
+        $tab3.style.visibility='hidden';
+        $tab4.style.visibility='hidden';
+        document.querySelector('.tab').innerHTML = $tab2;
+        $toggleTab2 = true;
+        $toggleTab1 = false;
+        $toggleTab3 = false;
+        $toggleTab4 = false;
+    }else if($toggleTab2 ===true){
+        $tab1.style.visibility='hidden';
+        $tab2.style.visibility='hidden';
+        $tab3.style.visibility='hidden';
+        $tab4.style.visibility='hidden';
+        $toggleTab2 = false;
+        $toggleTab1 = true;
+        $toggleTab3 = true;
+        $toggleTab4 = true;
+    }
+
+}
+let $toggleTab3 = false;
+let tab3 = function(){
+    
+    let $tab1 = document.querySelector('.tab1');
+    let $tab2 = document.querySelector('.tab2');
+    let $tab3 = document.querySelector('.tab3');
+    let $tab4 = document.querySelector('.tab4');
+
+    if($toggleTab3 ===false){
+        $tab1.style.visibility='hidden';
+        $tab2.style.visibility='hidden';
+        $tab3.style.visibility='visible';
+        $tab4.style.visibility='hidden';
+        $toggleTab3 = true;
+        $toggleTab1 = false;
+        $toggleTab2 = false;
+        $toggleTab4 = false;
+    }else if($toggleTab3 ===true){
+        $tab1.style.visibility='hidden';
+        $tab2.style.visibility='hidden';
+        $tab3.style.visibility='hidden';
+        $tab4.style.visibility='hidden';
+        $toggleTab3 = false;
+        $toggleTab1 = true;
+        $toggleTab2 = true;
+        $toggleTab4 = true;
+    }
+
+}
+let $toggleTab4 = false;
+let tab4 = function(){
+    
+    let $tab1 = document.querySelector('.tab1');
+    let $tab2 = document.querySelector('.tab2');
+    let $tab3 = document.querySelector('.tab3');
+    let $tab4 = document.querySelector('.tab4');
+
+    if($toggleTab4 ===false){
+        $tab1.style.visibility='hidden';
+        $tab2.style.visibility='hidden';
+        $tab3.style.visibility='hidden';
+        $tab4.style.visibility='visible';
+        $toggleTab4 = true;
+        $toggleTab1 = false;
+        $toggleTab2 = false;
+        $toggleTab3= false;
+    }else if($toggleTab4 ===true){
+        $tab1.style.visibility='hidden';
+        $tab2.style.visibility='hidden';
+        $tab3.style.visibility='hidden';
+        $tab4.style.visibility='hidden';
+        $toggleTab4 = false;
+        $toggleTab1 = true;
+        $toggleTab2 = true;
+        $toggleTab3 = true;
+    }
+
 }
